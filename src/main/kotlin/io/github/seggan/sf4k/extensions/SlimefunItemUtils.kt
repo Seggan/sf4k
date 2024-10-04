0b9e5bfca4637299b2cd44be2eec7781a9e47efc
Intybyte <vaan.testwork@gmail.com>
2024-10-04 18:41:50 +0200

    Documentation + Name refactoring

--- src/main/kotlin/io/github/seggan/sf4k/extensions/SlimefunItemUtils.kt
+++ src/main/kotlin/io/github/seggan/sf4k/extensions/SlimefunItemUtils.kt
@@ -3,9 +3,21 @@ package io.github.seggan.sf4k.extensions
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem
 import org.bukkit.inventory.ItemStack
 
-fun ItemStack.isSlimefun() = SlimefunItem.getByItem(this) != null
-fun ItemStack.asSlimefun() = SlimefunItem.getByItem(this)
+/**
+ * Checks if the item stack has slimefun data.
+ */
+fun ItemStack.isSlimefun() : Boolean = SlimefunItem.getByItem(this) != null
 
-inline fun <reified T : SlimefunItem> ItemStack.asSlimefun(): T {
-    return this.asSlimefun() as T
+/**
+ * Returns the associated SlimefunItem to an item stack,
+ * in case none is found the return value will be null.
+ */
+fun ItemStack.getSlimefun() : SlimefunItem? = SlimefunItem.getByItem(this)
+
+/**
+ * Behaves the same way as the extension function [ItemStack.getSlimefun],
+ * only difference is that it casts the result to a super class of a specified [SlimefunItem].
+ */
+inline fun <reified T : SlimefunItem> ItemStack.getSlimefun(): T {
+    return this.getSlimefun() as T
 }
