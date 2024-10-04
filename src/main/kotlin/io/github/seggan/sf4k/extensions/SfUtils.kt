f6e52c84114383951cee1f05d8a433b9e6eddf16
Seggan <seggan21@gmail.com>
2024-10-04 13:21:31 -0400

    Merge stuff

--- src/main/kotlin/io/github/seggan/sf4k/extensions/SfUtils.kt
+++
@@ -1,6 +0,0 @@
-package io.github.seggan.sf4k.extensions
-
-import io.github.thebusybiscuit.slimefun4.api.items.ItemSetting
-import kotlin.reflect.KProperty
-
-operator fun <T> ItemSetting<T>.getValue(thisRef: Any?, property: KProperty<*>): T = value
--- src/main/kotlin/io/github/seggan/sf4k/extensions/SlimefunItemUtils.kt
+++ src/main/kotlin/io/github/seggan/sf4k/extensions/SlimefunItemUtils.kt
@@ -1,7 +1,9 @@
 package io.github.seggan.sf4k.extensions
 
+import io.github.thebusybiscuit.slimefun4.api.items.ItemSetting
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItem
 import org.bukkit.inventory.ItemStack
+import kotlin.reflect.KProperty
 
 /**
  * Checks if the item stack has slimefun data.
@@ -10,14 +12,19 @@ fun ItemStack.isSlimefun() : Boolean = SlimefunItem.getByItem(this) != null
 
 /**
  * Returns the associated SlimefunItem to an item stack,
- * in case none is found the return value will be null.
+ * in case none is found, the return value will be null.
  */
 fun ItemStack.getSlimefun() : SlimefunItem? = SlimefunItem.getByItem(this)
 
 /**
  * Behaves the same way as the extension function [ItemStack.getSlimefun],
- * only difference is that it casts the result to a super class of a specified [SlimefunItem].
+ * only difference is that it casts the result to a subclass of the specified [SlimefunItem].
  */
 inline fun <reified T : SlimefunItem> ItemStack.getSlimefun(): T {
     return this.getSlimefun() as T
 }
+
+/**
+ * Allows [ItemSetting] to be used as a delegate
+ */
+operator fun <T> ItemSetting<T>.getValue(thisRef: Any?, property: KProperty<*>): T = value
